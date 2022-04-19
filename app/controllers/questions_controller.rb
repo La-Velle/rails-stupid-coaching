a11af03eae79d9db463003745da65a5d71981520
La-Velle <lavellesine@gmail.com>
2022-04-19 20:19:40 +0100

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:ask]
+    @answer = ""
+    if @ask.blank?
+      @answer = "ask me a question!"
+    elsif @ask == "I like easter bunnies!"
+      @answer = "super!"
+    elsif @ask.ends_with?("?")
+      @answer = "silly"
+    else
+      @answer = "great question!!"
+    end
   end
-
 end
